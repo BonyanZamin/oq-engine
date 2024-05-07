50473baec5cab53575681275359041be6759e8f9
BZPCE <148961127+BZPCE@users.noreply.github.com>
2024-05-07 15:32:42 +0330

    Ghasemi2009 modified
    ghasemi2009 coefficient table, and some other minor changes to the comments are modified, also the verification excel table is added.

--- openquake/hazardlib/gsim/ghasemi_2009.py
+++ openquake/hazardlib/gsim/ghasemi_2009.py
@@ -78,7 +78,10 @@ class Ghasemi2009(GMPE):
     #: The GMPE is derived from shallow earthquakes in California and Japan
     DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.ACTIVE_SHALLOW_CRUST
 
-    #: Supported intensity measure types are spectral acceleration
+    #: Supported intensity measure types is spectral acceleration
+    #: The attenuation relation developed by Ghasemi et. al estimates only the 'SA'.
+    #: Here in this code, the intensity measure of 'PGA' is also considered 
+    #: with the same coefficients as SA(0.05 s), in case of need.
     DEFINED_FOR_INTENSITY_MEASURE_TYPES = {PGA, SA}
 
     #: Supported intensity measure component is the average horizontal
@@ -92,8 +95,8 @@ class Ghasemi2009(GMPE):
     #: Required site parameters is Vs30
     REQUIRES_SITES_PARAMETERS = {'vs30'}
 
-    #: Required rupture parameters are magnitude and rake
-    REQUIRES_RUPTURE_PARAMETERS = {'mag', 'rake'}
+    #: Required rupture parameters are magnitude
+    REQUIRES_RUPTURE_PARAMETERS = {'mag'}
 
     #: Required distance measure is rupture distance
     REQUIRES_DISTANCES = {'rrup'}
@@ -118,11 +121,11 @@ class Ghasemi2009(GMPE):
             # Convert from common logarithm to natural logarithm
             sig[m] = np.log(10 ** C['sigma'])
 
-    """
-    The attenuation relation developed by Ghasemi et. al estimates only the 'SA'.
-    Here in this code, a line for intensity measure of 'PGA' is also considered 
-    with the same coefficients as SA(0.05 s), in case of need. 
-    """
+    
+    #: The attenuation relation developed by Ghasemi et. al estimates only the 'SA'.
+    #: Here in this code, a line for intensity measure of 'PGA' is also considered 
+    #: with the same coefficients as SA(0.05 s), in case of need. 
+
     COEFFS = CoeffsTable(sa_damping=5, table="""
     IMT     a1       a2       a3       a4       a6       a7       sigma
     PGA     0.868    0.405   -1.424    0.014    0.859    0.836    0.319               
@@ -130,18 +133,18 @@ class Ghasemi2009(GMPE):
     0.06    0.906    0.398   -1.440    0.015    0.944    0.911    0.322    
     0.07    0.957    0.394   -1.449    0.015    0.978    0.937    0.325
     0.08    0.700    0.387   -1.427    0.015    1.282    1.238    0.325
-    0.09    0.966    0.384   -1.413    0.016    1.005    1.005    0.326
-    0.10    0.904    0.380   -1.396    0.016    1.096    1.096    0.331
-    0.20    0.786    0.425   -1.215    0.015    0.748    0.748    0.319
-    0.30    0.432    0.474   -1.134    0.014    0.605    0.605    0.318
-    0.40    0.246    0.528   -1.080    0.011    0.289    0.289    0.327
-    0.50    0.003    0.571   -1.069    0.010    0.173    0.173    0.333
-    0.60   -0.118    0.608   -1.053    0.010   -0.037   -0.037    0.337
-    0.70   -0.234    0.635   -1.034    0.009   -0.194   -0.194    0.347
-    0.80   -0.331    0.673   -1.083    0.010   -0.300   -0.300    0.336
-    0.90   -0.459    0.706   -1.092    0.011   -0.424   -0.424    0.335
-    1.00   -0.567    0.727   -1.071    0.011   -0.533   -0.533    0.336
-    2.00   -1.209    0.876   -1.104    0.011   -1.183   -1.183    0.363
-    3.00   -1.436    0.920   -1.151    0.012   -1.411   -1.411    0.370
+    0.09    0.966    0.384   -1.413    0.016    1.046    1.005    0.326
+    0.10    0.904    0.380   -1.396    0.016    1.136    1.096    0.331
+    0.20    0.786    0.425   -1.215    0.015    0.663    0.748    0.319
+    0.30    0.432    0.474   -1.134    0.014    0.477    0.605    0.318
+    0.40    0.246    0.528   -1.080    0.011    0.135    0.289    0.327
+    0.50    0.003    0.571   -1.069    0.010    0.002    0.173    0.333
+    0.60   -0.118    0.608   -1.053    0.010   -0.209   -0.037    0.337
+    0.70   -0.234    0.635   -1.034    0.009   -0.361   -0.194    0.347
+    0.80   -0.331    0.673   -1.083    0.010   -0.450   -0.300    0.336
+    0.90   -0.459    0.706   -1.092    0.011   -0.570   -0.424    0.335
+    1.00   -0.567    0.727   -1.071    0.011   -0.678   -0.533    0.336
+    2.00   -1.209    0.876   -1.104    0.011   -1.291   -1.183    0.363
+    3.00   -1.436    0.920   -1.151    0.012   -1.515   -1.411    0.370
     """)
 
